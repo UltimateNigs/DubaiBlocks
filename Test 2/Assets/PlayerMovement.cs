4371436c65c2dc739a415637f4ac1bf3600ba421
Hasmit U <hasmituchil@gmail.com>
2016-02-19 13:42:07 +0530

    Slight Mods

---
+++ Test 2/Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine; 
+using System.Collections;
+public class PlayerMovement : MonoBehaviour { 
+	public float moveSpeed;  
+
+
+
+
+	 
+	void Start () {      
+		moveSpeed = 40f;
+
+	}          // Update is called once per frame    
+
+
+	void Update () {   
+
+		transform.Translate (moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+	} }
