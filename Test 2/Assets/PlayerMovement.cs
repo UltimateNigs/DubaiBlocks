b9048963fdd24fbcbea3eabb7dfd3430206994eb
Hasmit U <hasmituchil@gmail.com>
2016-02-19 13:44:48 +0530

    Minor mods

--- Test 2/Assets/PlayerMovement.cs
+++ Test 2/Assets/PlayerMovement.cs
@@ -2,21 +2,13 @@
 using System.Collections;
 public class PlayerMovement : MonoBehaviour { 
 	public float moveSpeed;  
-<<<<<<< HEAD
-=======
-	private float MaxSpeed=20f;
->>>>>>> 8b7321ed5eb3cf79fe1e773e56b0ac0e8be62586
 
 
 
 
 	 
 	void Start () {      
-<<<<<<< HEAD
 		moveSpeed = 40f;
-=======
-		moveSpeed = 2f;
->>>>>>> 8b7321ed5eb3cf79fe1e773e56b0ac0e8be62586
 
 	}          // Update is called once per frame    
 
